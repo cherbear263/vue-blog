45f136f1e5515a270d0827292b9f08b7d875644c
msjones3 <cheryl@realsocial.com.au>
2022-03-24 21:55:53 +1000

    add tags for each post

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,6 +1,10 @@
 <template>
   <h3 class="text-xl font-bold text-gray-500 mt-5">{{post.title}}</h3>
-  <p class="text-gray-500 font-sans text-sm">{{ snippet }}</p>
+  <p class="text-gray-500 font-sans text-sm mb-3">{{ snippet }}</p>
+  <span class="text-gray-400 uppercase tracking-tight font-semibold text-xs bg-cyan-100 rounded-lg px-2 py-1 mx-1"
+  v-for="tag in post.tags" :key="tag">
+    #{{tag}}
+  </span>
 </template>
 <script>
 import { computed } from '@vue/runtime-core'
