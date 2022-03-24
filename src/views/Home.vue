b746d90df7fd05533e8e68a354c10843dac2e8d4
msjones3 <cheryl@realsocial.com.au>
2022-03-24 21:36:52 +1000

    node server and async fetch

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,14 @@
   <div class="max-w-xl mx-auto mt-20">
     <h1 class="text-xl font-bold uppercase text-gray-600 text-center">Home</h1>
     <div class="post-list">
-      <PostList v-if="showPosts" :posts="posts" />
-      <div class="flex gap-2 justify-center">
-        <button class="bg-cyan-400 text-gray-600 mt-10 font-semibold px-3 py-2 rounded-md" @click="showPosts = !showPosts">toggle posts</button>
-        <button class="bg-pink-400 text-gray-600 mt-10 font-semibold px-3 py-2 rounded-md" @click="posts.pop()">delete a post</button>
-
+      <div v-if="error" class="text-pink-500 text-center mt-5">{{error}}</div>
+      <div v-else>
+        <div v-if="posts.length">
+          <PostList :posts="posts" />
+        </div>
+        <div v-else>Loading ...</div>
       </div>
+      
     </div>
   </div>
 </template>
@@ -21,15 +23,30 @@ export default {
   name: 'Home',
   components: { PostList },
   setup() {
-    const posts = ref([
-      { title: 'welcome to the blog', 
-        body: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?',
-        id: 1},
-      { title: 'top 5 CSS tips', body: 'lorem ipusm', id: 2},
-    ])
-    const showPosts = ref(true)
+    const posts = ref([])
+    const error = ref(null)
+  
 
-    return { posts, showPosts }
+    const load = async () => {
+      try {
+
+        const data = await fetch('http://localhost:3000/posts')
+        if (!data.ok) {
+          throw Error('no data available')
+
+        }
+        posts.value = await data.json()
+        
+      } catch(err) {
+        // update the ref error with the thrown message
+        error.value = err.message
+        console.log(error.value)
+
+      }
+    }
+    load()
+    return { posts, error}
   }
 }
+  
 </script>
