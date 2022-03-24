87bdde97999162aab4c22c3861c7a5013692d5c7
msjones3 <cheryl@realsocial.com.au>
2022-03-25 06:42:11 +1000

    composable for single post detail view

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,5 +1,7 @@
 <template>
+<router-link :to="{ name: 'Details', params: {id: post.id }}">
   <h3 class="text-xl font-bold text-gray-500 mt-5">{{post.title}}</h3>
+</router-link>
   <p class="text-gray-500 font-sans text-sm mb-3">{{ snippet }}</p>
   <span class="text-gray-400 uppercase tracking-tight font-semibold text-xs bg-cyan-100 rounded-lg px-2 py-1 mx-1"
   v-for="tag in post.tags" :key="tag">
---
+++ src/views/Details.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-if="error" class="text-pink-500 text-center mt-5">{{error}}</div>
+  <div v-if="post" class="max-w-xl mx-auto">
+    <h3 class="text-xl font-bold text-gray-500 mt-5">{{ post.title }}</h3>
+    <p class="text-gray-500 leading-5 mt-3 whitespace-pre-wrap">{{post.body}}</p>
+  </div>
+</template>
+
+<script>
+import getPost from '../composables/getPost'
+
+export default {
+  props: ['id'],
+  setup(props) {
+    const { post, error, load } = getPost(props.id)
+
+    load()
+
+    return { post, error}
+
+  }
+  
+}
+</script>
